c79f3b237587652ab624c0e92ade85acd684748a
ejedevopsprod <73623490+ejedevopsprod@users.noreply.github.com>
2022-03-16 16:29:38 -0600

    Update Main.cs

--- source/prueba/prueba.iOS/Main.cs
+++ source/prueba/prueba.iOS/Main.cs
@@ -14,6 +14,7 @@ namespace prueba.iOS
         {
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
+            [Obsolete]
             UIApplication.Main(args, null, "AppDelegate");
         }
     }
